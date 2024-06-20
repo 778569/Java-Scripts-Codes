34b451e85cf766c21bf7821e666c228cea46ed9b
kavinda <kavinda@digital-immersion.com>
2024-06-21 00:40:01 +0530

    variable

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
-console.log("Hope you are fine.");
+// console.log("Hope you are fine.");
+
+var firstName = 'Kavinda';
+// console.log(firstName)
+
+var lastName= 'Bandara';
+var age=26;
+var isOk=true;
+var job, height;
+
+
+
+
+
+
+
+// console.log("My name is " + firstName + " " + lastName)
+// console.log(`My age is ${age}`)
+// console.log('It is' + isOk)
+// console.log('I am a' + job)
+
+
+// console.log(typeof(firstName))
+// console.log(typeof(age))
+// console.log(typeof(isOk))
+// console.log(typeof(job))
+
