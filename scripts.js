6d544dfa4f4d2f2b9ef355d2e54474ec34d98f54
kavinda <kavinda@digital-immersion.com>
2024-06-22 17:32:52 +0530

    Arithmetic

--- scripts.js
+++ scripts.js
@@ -1,40 +1,85 @@
-// console.log("Hope you are fine.");
+// // console.log("Hope you are fine.");
 
-var firstName = 'Kavinda';
-// console.log(firstName)
+// var firstName = 'Kavinda';
+// // console.log(firstName)
 
-var lastName= 'Bandara';
-var age=26;
-var isOk=true;
-var job, height;
-
-
+// var lastName= 'Bandara';
+// var age=26;
+// var isOk=true;
+// var job, height;
 
 
 
 
 
-// console.log("My name is " + firstName + " " + lastName)
-// console.log(`My age is ${age}`)
-// console.log('It is' + isOk)
-// console.log('I am a' + job)
 
 
-// console.log(typeof(firstName))
-// console.log(typeof(age))
-// console.log(typeof(isOk))
-// console.log(typeof(job))
-
-job = 'Doctor';
-lastName="Perera"
-
-console.log('I am '+ ' ' + lastName + ' ' +'My occupation is ' + job)
+// // console.log("My name is " + firstName + " " + lastName)
+// // console.log(`My age is ${age}`)
+// // console.log('It is' + isOk)
+// // console.log('I am a' + job)
 
 
-// alert('My name is ' + ' ' + firstName + ' '+ 'I am '+ age + ' years old.!!!'
-// );
+// // console.log(typeof(firstName))
+// // console.log(typeof(age))
+// // console.log(typeof(isOk))
+// // console.log(typeof(job))
+
+// job = 'Doctor';
+// lastName="Perera"
+
+// console.log('I am '+ ' ' + lastName + ' ' +'My occupation is ' + job)
 
 
-height = prompt('What is your height.? ')
+// // alert('My name is ' + ' ' + firstName + ' '+ 'I am '+ age + ' years old.!!!'
+// // );
 
-alert("My height is " + height+" feet")
+
+// height = prompt('What is your height.? ')
+
+// alert("My height is " + height+" feet")
+
+
+// Arithmetic
+
+var myAge, yourAge,year,result;
+ 
+myAge=26;
+yourAge=20;
+year =2021;
+
+result = year - myAge;
+console.log(`I was born in ${result}`)
+
+result = year + 10 ;
+console.log(`After 10 years ${result}`)
+
+
+console.log(` ${myAge * yourAge}`)
+
+console.log(` ${(myAge / yourAge)}`)
+
+console.log(` ${(myAge % yourAge)}`)
+
+console.log(` ${(myAge ** 2)}`)
+
+console.log(` ${(myAge++)}`)
+
+console.log(` ${(++myAge)}`)
+
+console.log(` ${(myAge--)}`)
+
+console.log(` ${(--myAge)}`)
+
+var age = 50;
+age += 10; 
+console.log(age)
+
+age -= 10;
+console.log(age)
+
+age *= 2;
+console.log(age)
+
+age /= 2;
+console.log(age)
