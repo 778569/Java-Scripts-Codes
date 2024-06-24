507acd1a1cf1daf489d6c36d845dfc39a3095714
kavinda <kavinda@digital-immersion.com>
2024-06-24 21:28:14 +0530

    conditions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,67 @@
-console.log("Hope you are fine.");
+// console.log("Hope you are fine.");
+
+// var a=10;
+// var b=10;
+
+// if(a<b){
+//     console.log("b is greater than a")
+// }
+// else if(a>b){
+//     console.log("a is grater than b")
+// } else {
+//     console.log("a equal to b")
+// }
+
+// var a = 20;
+// var b= '20';
+// var c=5;
+// var d=15;
+
+// if(a === b){
+//         if(a === b){
+//                 console.log("Both value and type of a is equal to b")
+//         } else if (a !== b){
+//                 console.log("Only value of type of a is equal to b")
+//         }
+// }
+
+// else if(a>=10){
+//     if(a>10){
+//         console.log("a is greater than 10")
+//     }
+//     else if(a == 10){
+//         console.log("a is equal to 10")
+//     }
+
+// }
+
+// else if(a != c){
+//     if(a<c){
+//         console.log('a is less than c')
+//     }
+//     else{
+//         console.log('a is grater than c')
+//     }
+
+// }
+
+// //ternary operator
+
+// c<=5 ? console.log("c less than a") : console.log("c is greater than a")
+
+var a = 20
+var b = 10
+var c= 5
+
+if(a == 10 && a==b ){
+    console.log("Its good")
+}
+else if(a>=10 || c>b)
+{
+console.log("It is normal")
+}
+else if(c != a){
+    console.log("it is bad")
+}else{
+    
+}
