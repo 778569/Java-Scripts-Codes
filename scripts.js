f97238cdb16f1999a4f1b09f498f06d06ed78600
kavinda <kavinda@digital-immersion.com>
2024-06-25 21:11:39 +0530

    Function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,38 @@
-console.log("Hope you are fine.");
+function callFunction(){
+  console.log("Function called.!!!")
+}
+
+// callFunction()
+
+function addNumbers(num1, num2){
+    var ans = num1 + num2;
+    console.log(ans)
+}
+
+addNumbers(12, 36.5);
+
+
+function subNumbers(num1,num2){
+    return num1 - num2;
+
+}
+
+
+var subnum = subNumbers(10,5)
+console.log(subnum)
+
+
+function calculateAge(birthday = 1994){
+  return 2024- birthday
+}
+
+
+// console.log("Kasun is " + calculateAge(1990) + " years old")
+
+function calculateRetiringYear(FirstName, birthYear){
+var age = calculateAge(birthYear)
+var retiredYear = 60 - age;
+console.log(`${FirstName} retires in ${retiredYear} of ${2024+retiredYear}`)
+}
+
+calculateRetiringYear('Kavinda', 1994)
