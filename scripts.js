9293d9b1334e53c6caa36a35a44fba2f460ce874
kavinda <kavinda@digital-immersion.com>
2024-06-25 05:48:30 +0530

    switch case

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-console.log("Hope you are fine.");
+var empName = "Kavinda"
+var jobRole="engineer"
+
+
+switch(jobRole.toLocaleLowerCase()){
+case 'teacher':
+    console.log(empName + "is a Teacher");
+    break;
+
+case 'doctor':
+    console.log(empName + "is a Doctor")
+    break;
+
+default:
+    console.log(empName + "is a engineer")
+    break;
+}
