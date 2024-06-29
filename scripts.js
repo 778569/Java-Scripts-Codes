391ad3bbcf4bf0c1ac094c6f332ca1fda91d2181
kavinda <kavinda@digital-immersion.com>
2024-06-29 15:17:03 +0530

    Object

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
-console.log("Hope you are fine.");
+Kasun = {
+    fullName: 'Kasun Samantha',
+    mass: 50,
+    height:6,
+    BMI : function(){
+return (this.mass / this. height ** 2)
+    }
+}
+
+Chamal = {
+    fullName: 'Chamal Herath',
+    mass: 70,
+    height:5.5,
+    BMI : function(){
+        return (this.mass / this.height ** 2)
+            }
+}
+
+
+console.log(Kasun.BMI())
+console.log(Chamal.BMI())
+
+if(Kasun.BMI()>Chamal.BMI()){
+    console.log("Kasun has higher Bmi")
+
+} else{
+    console.log("Chamal has higher Bmi")
+}
