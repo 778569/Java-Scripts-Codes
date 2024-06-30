00501073012aa45ce0270e3c3b10dce87e224a8a
kavinda <kavinda@digital-immersion.com>
2024-06-30 18:27:11 +0530

    Scopes local and globle variables

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-console.log("Hope you are fine.");
+// console.log("Hope you are fine.");
+
+
+function addNum(){
+    //Local variable
+    var a = 10
+    var b = 20
+    console.log(a+b)
+}
+
+addNum()
+
+
+var subject = 'Maths'
+
+function mySub(){
+console.log("My favourite subject is " + subject)
+subject = 'science'
+}
+
+mySub()
+mySub()
