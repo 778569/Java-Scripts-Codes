239fa08c7a3a3e247ebad42b22c6a23fff422916
kavinda <kavinda@digital-immersion.com>
2024-06-30 19:08:41 +0530

    Exercise 03

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
-console.log("Hope you are fine.");
+var tipbill = []
+var totalBill = []
+
+
+var billCal = {
+    bill : [1230,982,1640,1460,994],
+    tip  : function tipCal(){
+        for(var i of this.bill){
+            console.log(i)
+if(i <= 1000){
+var mytip =  i * (20/100)
+}
+else if(i<1500){
+    var mytip = i * (15/100)
+}else{
+    var mytip = i * (10/100)
+}
+tipbill.push(mytip)
+totalBill.push(mytip + i)
+        }
+    }
+}
+
+
+billCal.tip()
+console.log(tipbill)
+console.log(totalBill)
