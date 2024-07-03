b39b2be6d5b8c5cd6116593a107edf51847b9421
kavinda <kavinda@digital-immersion.com>
2024-07-03 11:21:05 +0530

    js element handler

--- scripts.js
+++ scripts.js
@@ -90,30 +90,84 @@
 
 //    using Query selector
 
-var navb = document.querySelector('#navbar')
-navb.style.border = 'solid 1px #000'
+// var navb = document.querySelector('#navbar')
+// navb.style.border = 'solid 1px #000'
 
 
-var input = document.querySelector('input')
-input.value="bag" 
+// var input = document.querySelector('input')
+// input.value="bag" 
 
-var item = document.querySelector('.items');
-item.style.backgroundColor="red"
+// var item = document.querySelector('.items');
+// item.style.backgroundColor="red"
 
-var it = document.querySelector('div .frm #frmt')
-it.style.color='orange'
-
-
-document.querySelectorAll('.items')[1].style.color="purple"
-
-var odd = document.querySelectorAll('li:nth-child(odd)')
-for(var i of odd){
-    i.style.backgroundColor='green'
-}
+// var it = document.querySelector('div .frm #frmt')
+// it.style.color='orange'
 
 
+// document.querySelectorAll('.items')[1].style.color="purple"
 
-var even = document.querySelectorAll('li:nth-child(even)')
-for(var i of even){
-    i.style.backgroundColor='blue'
-}
+// var odd = document.querySelectorAll('li:nth-child(odd)')
+// for(var i of odd){
+//     i.style.backgroundColor='green'
+// }
+
+
+
+// var even = document.querySelectorAll('li:nth-child(even)')
+// for(var i of even){
+//     i.style.backgroundColor='blue'
+// }
+
+// Traversing the Dom
+
+var itemlist = document.querySelector('#li-items')
+// console.log(itemlist.parentNode)
+// console.log(itemlist.parentNode.parentNode)
+
+// itemlist.parentNode.style.backgroundColor='red'
+// console.log(itemlist.parentElement)
+
+
+// console.log(itemlist.childNodes)
+// console.log(itemlist.childNodes[1])
+
+
+// console.log(itemlist.children)
+// itemlist.children[0].style.backgroundColor='red'
+// console.log(itemlist.lastElementChild)
+
+// console.log(itemlist.nextElementSibling)
+// itemlist.nextElementSibling.style.color='red'
+
+
+// console.log(itemlist.previousElementSibling)
+// itemlist.previousElementSibling.style.color='red'
+
+
+
+// create element 
+
+// create an Input
+var input = document.createElement('input')
+console.log(input)
+input.className ='test'
+input.id='last'
+
+
+input.setAttribute('type','text')
+// input.setAttribute('type')
+
+
+var newDiv = document.createElement('div')
+var newText = document.createTextNode('How to work ?')
+
+newDiv.appendChild(newText)
+console.log(newDiv)
+
+// insert created element to the document
+
+var form = document.querySelector('.frm form')
+var button = document.querySelector('.frm form button')
+
+input.style.backgroundColor='yellow'
+form.insertBefore(input,button)
